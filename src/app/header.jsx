f75c0f1db04df84ea466a9ad16877804b4e1994b
shahbaz-ramzan <mshahbazramzan952@gmail.com>
2023-12-01 18:56:22 +0500

    first commit

---
+++ src/app/header.jsx
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function Header() {
+  return (
+      <>
+          <nav>
+              <li><Link href="/">OFFERS</Link></li>
+              <li><Link href="/">MOST WANTED</Link></li>
+              <li><Link href="/">GARDENING</Link></li>
+              <li><Link href="/">COMPOSTING</Link></li>
+              <li><Link href="/">WATER SAVING</Link></li>
+              <li><Link href="/">RECYCLING</Link></li>
+              <li><Link href="/">BRANDS</Link></li>
+              <li><Link href="/">LEARN</Link></li>
+              <li><Link href="/">EARN REWARDS</Link></li>
+              <li><Link href="/">SUPPORT</Link></li>
+          </nav>
+      </>
+  )
+}
